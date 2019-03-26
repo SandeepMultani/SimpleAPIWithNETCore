fee652fd7bc07a8c20562c1421921924c4919e7c
SandeepMultani <Sandeep.Multani@bksb.co.uk>
2019-03-26 16:56:33 +0000

    code

---
+++ CityInfo.API/Migrations/20190326161040_CityInfoDb_AddedCityCode.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CityInfo.API.Migrations
+{
+    public partial class CityInfoDb_AddedCityCode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CityCode",
+                table: "Cities",
+                maxLength: 5,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CityCode",
+                table: "Cities");
+        }
+    }
+}
